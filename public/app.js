e962ea5c56216183e8aba07e6d11f7b3e1b025ed
David Wu <davidjwu92@gmail.com>
2019-12-27 12:12:31 -0800

    Cards Created

--- public/app.js
+++ public/app.js
@@ -15,7 +15,23 @@ const getArticles = () => {
     .then(({data}) => {
       console.log(data)
       //create cards for articles
-
+      data.forEach((elem, i)=>{
+        let articleCard = document.createElement('div')
+        articleCard.className = "row"
+        articleCard.innerHTML = 
+       `<div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">Article #${i + 1}: ${elem.title}</span>
+              <p>${elem.summary}</p>
+            </div>
+            <div class="card-action">
+              <a href=${elem.url}>${elem.url}</a>
+            </div>
+          </div>
+        </div>`
+        document.getElementById('articles').append(articleCard)
+      })
     })
     .catch(e=>console.error(e))
 }
